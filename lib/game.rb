0c331f77eb393f079b28e4616615552a5c7e3d7f
Thomas Christopher <thomas.j.christopher@gmail.com>
2019-09-05 14:29:06 +0100

    pick a random word from 5desk.txt

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,11 @@
 puts "Hangman initialized."
 
-contents = File.read "5desk.txt"
-puts contents
+lines = File.readlines "5desk.txt"
+
+def pick_random_word(lines)
+  word = lines.sample
+  pick_random_word(lines) unless word.length > 5 && word.length < 12
+  word
+end
+
+puts pick_random_word(lines)
